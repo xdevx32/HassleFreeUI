44a12d58781095dbcfe739d63f98257c1ee3e1fe
MetalDestructor <rapid3fighter@hotmail.com>
2019-08-17 10:55:16 +0300

    Add navigation

--- hassle-free-ui/src/app/app.module.ts
+++ hassle-free-ui/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatListModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatIconModule,
+  MatGridListModule,
+  MatAutocompleteModule,
+  MatChipsModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HassleFreeNavComponent } from './hassle-free-nav/hassle-free-nav.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HassleFreeNavComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatGridListModule,
+    MatAutocompleteModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ hassle-free-ui/src/app/hassle-free-nav/hassle-free-nav.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hassle-free-nav',
+  templateUrl: './hassle-free-nav.component.html',
+  styleUrls: ['./hassle-free-nav.component.css']
+})
+export class HassleFreeNavComponent implements OnInit {
+  userData: any;
+
+  ngOnInit() {
+    
+  }
+}
